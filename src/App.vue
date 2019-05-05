ef87ad9b6504d54817bf8680a8107be22cdbc2a7
nduhiu17 <nduhiu254@gmail.com>
2019-05-05 09:57:26 +0300

    create forums folder and set routes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,145 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="nav-bar">
+      <div class="nav-logo">
+        <router-link to="/">Coffee Bag</router-link>
+      </div>
+      <div class="nav-details">
+        <div class="nav-about">
+          <router-link to="/about">About</router-link>
+        </div>
+        <div class="nav-documentation">Documentation</div>
+        <div class="nav-contacts">Contacts</div>
+      </div>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,
+html {
+  margin: 0;
+  padding: 5px;
+  font-family: Open Sans, Segoe UI, Helvetica Neue, Helvetica, Raleway, Arial,
+    sans-serif;
+}
+
+.nav-bar a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.nav-bar {
+  background-color: #333;
+  border-bottom: 1px solid #d6d7d9;
+  box-shadow: none;
+  color: #ffffff;
+  height: 4em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-details {
+  display: flex;
+  flex-direction: row;
+}
+
+.nav-details > div {
+  padding: 10px;
+}
+
+.nav-logo {
+  font-weight: 600;
+  font-size: 18px;
+  margin-left: 10px;
+}
+
+.header {
+  height: 25vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-bottom: 1px solid #d6d7d9;
+  font-weight: 800;
+  font-size: 22px;
+}
+
+.forum-item {
+  background: #fff;
+  border-top: 1px solid #d6d7d9;
+  padding: 14px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.forum-details {
+  display: flex;
+}
+
+.forum-content-details {
+  display: flex;
+  justify-content: flex-end;
+  font-size: 12px;
+}
+
+.forum-item > div {
+  padding: 10px;
+}
+
+.forum-details > div {
+  padding: 10px;
+}
+
+.forum-content-details > div {
+  padding: 10px;
+}
+
+.forum-title {
+  color: #0291db;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 21px;
+  margin-bottom: 6px;
+}
+
+.forum-views,
+.forum-comments {
+  background: rgba(2, 145, 219, 0.9);
+  border-radius: 6px;
+  bottom: 6px;
+  color: #fff;
+  font-size: 14px;
+  margin: 0;
+  max-height: 70px;
+  display: flex;
+  align-items: center;
+}
+
+.forum-comments {
+  margin-left: 10px;
+}
+
+.new-forum {
+  background: #f52c6e;
+  border: 1px solid #f52c6e;
+  border-radius: 6px;
+  color: #fff;
+  font-family: Open Sans, Segoe UI, Helvetica Neue, Helvetica, Raleway, Arial,
+    sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 36px;
+  min-height: 36px;
+  padding: 0 12px;
+  text-decoration: none;
+  transition: background 0.1s ease, border 0.1s ease;
+}
+
+.button-wrapper {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Forums/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Forums from "@/components/Forums.vue";
+
+export default {
+  name: "home",
+  components: {
+    Forums
+  }
+};
+</script>
